3c97f3322728684ca3fb682a0ac4b30014f3ae0b
Hugo D. (jabesq) <jabesq@gmail.com>
2018-02-01 14:54:38 +0100

    AppDaemon Harmony support
    Harmony automations are much easier to manage with AppDaemon
    Bug fixing
    
    Signed-off-by: Hugo D. (jabesq) <jabesq@gmail.com>

---
+++ appdaemon/apps/harmony.py
@@ -0,0 +1,31 @@
+import appdaemon.appapi as appapi
+
+
+class HarmonySceneHandler(appapi.AppDaemon):
+
+    def initialize(self):
+        self.listen_state(self.activate_scene_input, "remote.harmony_hub",
+                                  attribute="current_activity",
+                                  new=self.args["scene"])
+        self.listen_state(self.deactivate_scene_input, "remote.harmony_hub",
+                                  attribute="current_activity",
+                                  old=self.args["scene"])
+        self.listen_state(self.activate_scene, self.args["input_boolean"],
+                            new='on')
+
+    def activate_scene_input(self, entity, attribute, old, new, kwargs):
+        #self.log_notify("State change: {} to {}".format(entity, new))
+        self.log("Scène: {} démarré depuis Harmony".format(self.args["scene"]))
+        if self.get_state(self.args["input_boolean"]) == 'off':
+            self.turn_on(self.args["input_boolean"])
+
+    def deactivate_scene_input(self, entity, attribute, old, new, kwargs):
+        #self.log_notify("State change: {} to {}".format(entity, new))
+        self.log("Scène: {} arrêtée depuis Harmony".format(self.args["scene"]))
+        if self.get_state(self.args["input_boolean"]) == 'on':
+            self.turn_off(self.args["input_boolean"])
+
+    def activate_scene(self, entity, attribute, old, new, kwargs):
+        self.log("Démarrer {}".format(self.args["scene"]))
+        if self.entities.remote.harmony_hub.attributes.current_activity != self.args["scene"]:
+            self.turn_on("remote.harmony_hub", activity=self.args["scene"])
