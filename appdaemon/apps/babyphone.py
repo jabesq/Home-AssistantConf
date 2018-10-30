ae04fc8386f0f257002dff7cd0d4e89bcdaf5d4e
Hugo D. (jabesq) <jabesq@gmail.com>
2018-10-30 20:42:19 +0100

    Appdaemon: Update babyphone scripts
    Signed-off-by: Hugo D. (jabesq) <jabesq@gmail.com>

--- appdaemon/apps/babyphone.py
+++ appdaemon/apps/babyphone.py
@@ -19,11 +19,17 @@ class BabyPhoneHandler(hass.Hass):
     """Event listener for babyphone events."""
 
     def initialize(self):
+        self.register_constraint("media_player_selection")
         self.listen_state(
-            self.babyphone_alarm_on, "binary_sensor.ffmpeg_noise", new="on"
+            self.babyphone_alarm_on, "binary_sensor.ffmpeg_noise", new="on",
+            media_player_selection=1
             )
         self.listen_state(
-            self.babyphone_alarm_off, "binary_sensor.ffmpeg_noise", new="off"
+            self.babyphone_alarm_off, "binary_sensor.ffmpeg_noise", new="off",
+            media_player_selection=1
+            )
+        self.listen_state(
+            self.stop_babyphone, self.args["input_boolean"], new="off"
             )
 
     def babyphone_alarm_on(self, entity, attribute, old, new, kwargs):
@@ -53,16 +59,40 @@ class BabyPhoneHandler(hass.Hass):
             self.call_service("media_player/sonos_restore",
                                entity_id = self.args["media_player"])
 
-    def light_alarm(self, kwargs):
-        self.toggle(self.args["alarm_light"])
-        if self.alarm_light_on:
-            self.run_in(self.light_alarm, 1)
-
-    def stop_alarm(self):
-        self.alarm_light_on = False
+    def stop_babyphone(self, entity, attribute, old, new, kwargs):
+        self.log("Babyphone stopped")
+        self.call_service("media_player/media_pause",
+                          entity_id = self.args["media_player"])
+        if "sonos" in self.args:
+            self.call_service("media_player/sonos_restore",
+                               entity_id = self.args["media_player"])
 
     def media_player_selection(self, value):
         if self.get_state(self.args["input_boolean"]) == 'on':
             return True
         else:
             return False
+
+class BabyPhoneLightHandler(hass.Hass):
+    def initialize(self):
+        self.listen_state(
+            self.start_alarm, "binary_sensor.ffmpeg_noise", new="on", old="off"
+            )
+        self.listen_state(
+            self.stop_alarm, "binary_sensor.ffmpeg_noise", old="on"
+            )
+
+    def start_alarm(self, entity, attribute, old, new, kwargs):
+        self.alarm_light_on = True
+        self.turn_on(self.args["alarm_light"], color_name="red")
+        self.light_alarm(kwargs)
+
+    def light_alarm(self, kwargs):
+        self.toggle(self.args["alarm_light"])
+        if self.alarm_light_on:
+            self.run_in(self.light_alarm, 1)
+        else:
+            self.turn_on(self.args["alarm_light"], color_name="green")
+
+    def stop_alarm(self, entity, attribute, old, new, kwargs):
+        self.alarm_light_on = False
